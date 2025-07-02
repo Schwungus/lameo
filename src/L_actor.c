ba21f840d243f3e5dc46e6238b90b7f679b9ce24
Can't Sleep <localamnesiac@gmail.com>
2025-07-02 15:54:46 +0300

    Update L_actor.c

--- src/L_actor.c
+++ src/L_actor.c
@@ -1,3 +1,17 @@
 #include "L_actor.h"
+#include "L_log.h"
+#include "L_memory.h"
 
-static struct ActorType* actor_types = NULL;
+static struct HashMap* actor_types = NULL;
+
+void actor_init() {
+    actor_types = create_hash_map();
+
+    INFO("Opened");
+}
+
+void actor_teardown() {
+    destroy_hash_map(actor_types, true);
+
+    INFO("Closed");
+}
